97739202017ca8840df205d5d23200926bc01def
jeyeldotgit <jeyel.git@gmail.com>
2026-05-16 16:42:06 +0800

    Init Commit: First Ever MVC for ASPNET ever created in my tech career

---
+++ Controllers/BlogController.cs
@@ -0,0 +1,75 @@
+﻿using BlogPostMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlogPostMVC.Controllers
+{
+    public class BlogController : Controller
+    {
+        public IActionResult Index()
+        {
+            var posts = new List<Post>
+            {
+                new Post
+                {
+                    Id = 1,
+                    Title = "My First Blog Post",
+                    Content = "This is the content of my first post.",
+                    Author = "Josh Lester Gruta"
+                },
+                new Post
+                {
+                    Id = 2,
+                    Title = "Learning ASP.NET Core MVC",
+                    Content = "MVC stands for Model, View, and Controller.",
+                    Author = "Josh Lester Gruta"
+                },
+                new Post
+                {
+                    Id = 3,
+                    Title = "Building a Blog App",
+                    Content = "This is a simple blog application using mock data.",
+                    Author = "Josh Lester Gruta"
+                }
+            };
+
+            return View(posts);
+        }
+
+        public IActionResult Details(int id)
+        {
+            var posts = new List<Post>
+            {
+                new Post
+                {
+                    Id = 1,
+                    Title = "My First Blog Post",
+                    Content = "This is the content of my first post.",
+                    Author = "Josh Lester Gruta"
+                },
+                new Post
+                {
+                    Id = 2,
+                    Title = "Learning ASP.NET Core MVC",
+                    Content = "MVC stands for Model, View, and Controller.",
+                    Author = "Josh Lester Gruta"
+                },
+                new Post
+                {
+                    Id = 3,
+                    Title = "Building a Blog App",
+                    Content = "This is a simple blog application using mock data.",
+                    Author = "Josh Lester Gruta"
+                }
+            };
+
+            var post = posts.FirstOrDefault(p => p.Id == id);
+
+            if (post == null)
+            {
+                return NotFound();
+            }
+
+            return View(post);
+        }
+    }
+}
